2581053a8cd14bec9c74c362a44bfa61bf4e15a2
Charles Marcoin <charles.marcoin@gmail.com>
2022-08-19 14:47:22 +0200

    added router and demo components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header";
+
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/sign_in" element={<Login />} exact />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>404 page non trouvée</p>
+            </main>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
