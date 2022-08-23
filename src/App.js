262ad2ddc2acc634b3def0944c3debafe756adf5
Charles Marcoin <charles.marcoin@gmail.com>
2022-08-23 22:14:47 +0200

    refacto router, now in index.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Banner from "./components/Banner/Banner";
-
-import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
-import '@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.css';
-import '@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.css';
-import '@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.css';
-
-import 'font-awesome/css/font-awesome.min.css'
-import "./App.scss";
-
-// import 'font-awesome/css/font-awesome.min.css'
-import Login from "./pages/Login";
-import AcceptInvitation from "./pages/AcceptInvitation";
-import Appointments from "./pages/Appointments";
-import Agents from "./pages/Agents";
-import Account from "./pages/Account";
-import Appointment from "./pages/Appointment";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
-        <Banner />
-        <div className="static-wrapper">
-          <Routes>
-            <Route path="/sign_in" element={<Login />} exact />
-            <Route
-              path="/accept_invitation"
-              element={<AcceptInvitation />}
-              exact
-            />
-            <Route path="/appointments" element={<Appointments />} exact />
-            <Route path="/agent" element={<Agents />} exact />
-            <Route path="/convict" element={<Account />} exact />
-            <Route path="/appointment" element={<Appointment />} exact />
-            <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>404 page non trouvée</p>
-                </main>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+     <Banner />
+     <Outlet />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import App from "./App";
+import Login from "./pages/Login";
+import AcceptInvitation from "./pages/AcceptInvitation";
+import Appointments from "./pages/Appointments";
+import Agents from "./pages/Agents";
+import Account from "./pages/Account";
+import Appointment from "./pages/Appointment";
 import reportWebVitals from "./reportWebVitals";
+
+import "@gouvfr/dsfr/dist/dsfr/dsfr.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-system/icons-system.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-user/icons-user.css";
+import "@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.css";
+
+import "font-awesome/css/font-awesome.min.css";
+import "./App.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div className="static-wrapper">
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="sign_in" element={<Login />} exact />
+            <Route
+              path="accept_invitation"
+              element={<AcceptInvitation />}
+              exact
+            />
+            <Route path="appointments" element={<Appointments />} exact />
+            <Route
+              path="appointments/:appointmentId"
+              element={<Appointment />}
+            />
+            <Route path="agent" element={<Agents />} exact />
+            <Route path="convict" element={<Account />} exact />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>404 page non trouvée</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   </React.StrictMode>
 );
 
