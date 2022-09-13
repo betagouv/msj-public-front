7e3dffeefeceb03502b49960436de15f71ac6c70
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-13 10:42:09 +0200

    added error handling for accept invitation form

--- src/pages/AcceptInvitation.jsx
+++ src/pages/AcceptInvitation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import useForm from 'shared/hooks/form-hook';
@@ -11,10 +11,18 @@ import {
   VALIDATOR_ONE_SPECIAL_CHAR,
   VALIDATOR_IDENTICAL,
 } from 'shared/utils/validators';
+import { useAuth } from 'shared/hooks/auth-hook';
+
+import Alert from 'shared/components/Alerts/Alert';
 
 function AcceptInvitation() {
   const [searchParams] = useSearchParams();
   const invitationToken = searchParams.get('token');
+
+  const { login } = useAuth();
+
+  // const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const [formState, inputHandler] = useForm({
     password: {
@@ -27,10 +35,15 @@ function AcceptInvitation() {
     },
   }, false);
 
+  const onCloseAlertHandler = () => {
+    setError(null);
+  };
+
   const acceptInvitationSubmitHandler = async (e) => {
     e.preventDefault();
 
     try {
+      // setIsLoading(true);
       const response = await fetch(`${process.env.REACT_APP_BACKEND_HOST}/api/users/signup`, {
         method: 'POST',
         headers: {
@@ -42,121 +55,132 @@ function AcceptInvitation() {
         }),
       });
 
-      const data = response.json();
-      console.log(data);
+      const resData = await response.json();
+
+      if (!resData.ok) {
+        throw new Error(resData.message);
+      }
+
+      setIsLoading(false);
+      login(resData);
     } catch (err) {
-      console.log(err);
+      setIsLoading(false);
+      setError(err || "Une erreur s'est produite, contactez l'administrateur du site");
     }
   };
 
-  console.log(formState);
-
   return (
-    <div className="fr-container fr-py-4w px-12 lg:px-72">
-      <h2 className="text-2xl mb-4">Je choisis mon mot de passe</h2>
 
-      <form
-        acceptCharset="UTF-8"
-        onSubmit={acceptInvitationSubmitHandler}
-      >
-        <TextInput
-          label="Mot de passe"
-          type="password"
-          id="password"
-          hint="10 caractères, avec une majuscule, un chiffre et un caractère spécial"
-          required
-          autoComplete="current-password"
-          onInput={inputHandler}
-          errorMessage="Le mot de passe doit contenir 10 caractères, avec une majuscule, un chiffre et un caractère spécial"
-          validators={[
-            VALIDATOR_REQUIRE(),
-            VALIDATOR_MINLENGTH(10),
-            VALIDATOR_ONE_UPPERCASE(),
-            VALIDATOR_ONE_DIGIT(),
-            VALIDATOR_ONE_SPECIAL_CHAR(),
-          ]}
-        />
+    <>
+      <Alert title={error?.message} show={!!error} type="error" closable onClose={onCloseAlertHandler} />
 
-        <TextInput
-          label="Confirmation de mot de passe"
-          id="passwordConfirmation"
-          required
-          autoComplete="current-password"
-          errorMessage="Les mots de passe ne correspondent pas"
-          onInput={inputHandler}
-          validators={[
-            VALIDATOR_IDENTICAL(formState.password),
-          ]}
-        />
-
-        <button
-          type="submit"
-          name="commit"
-          className="fr-btn mb-4"
-          data-disable-with="Je valide"
-          disabled={!formState.isValid}
+      <div className="fr-container fr-py-4w px-12 lg:px-72">
+        <h2 className="text-2xl mb-4">Je choisis mon mot de passe</h2>
+        <form
+          acceptCharset="UTF-8"
+          onSubmit={acceptInvitationSubmitHandler}
         >
-          Je valide
-        </button>
-      </form>
+          <TextInput
+            label="Mot de passe"
+            type="password"
+            id="password"
+            hint="10 caractères, avec une majuscule, un chiffre et un caractère spécial"
+            required
+            autoComplete="current-password"
+            onInput={inputHandler}
+            errorMessage="Le mot de passe doit contenir 10 caractères, avec une majuscule, un chiffre et un caractère spécial"
+            validators={[
+              VALIDATOR_REQUIRE(),
+              VALIDATOR_MINLENGTH(10),
+              VALIDATOR_ONE_UPPERCASE(),
+              VALIDATOR_ONE_DIGIT(),
+              VALIDATOR_ONE_SPECIAL_CHAR(),
+            ]}
+          />
 
-      <h2 className="text-xl mb-4 mt-10">Mon compte est désormais actif</h2>
+          <TextInput
+            label="Confirmation de mot de passe"
+            id="passwordConfirmation"
+            required
+            autoComplete="current-password"
+            errorMessage="Les mots de passe ne correspondent pas"
+            onInput={inputHandler}
+            validators={[
+              VALIDATOR_IDENTICAL(formState.password),
+            ]}
+          />
 
-      <div className="fr-grid-row fr-grid-row--gutters">
-        <div className="fr-col-6">
-          <div className="fr-tile">
-            <div className="fr-tile__body">
-              <h5 className="fr-tile__title">Je peux suivre mes rendez-vous</h5>
+          <button
+            type="submit"
+            name="commit"
+            className="fr-btn mb-4"
+            data-disable-with="Je valide"
+            disabled={!formState.isValid}
+          >
+            Je valide
+          </button>
+        </form>
+
+        <h2 className="text-xl mb-4 mt-10">Mon compte est désormais actif</h2>
+
+        <div className="fr-grid-row fr-grid-row--gutters">
+          <div className="fr-col-6">
+            <div className="fr-tile">
+              <div className="fr-tile__body">
+                <h5 className="fr-tile__title">Je peux suivre mes rendez-vous</h5>
+              </div>
+              <div className="fr-tile__img">
+                <span
+                  className="fr-fi-calendar-line icon-xl"
+                  aria-hidden="true"
+                />
+              </div>
             </div>
-            <div className="fr-tile__img">
-              <span
-                className="fr-fi-calendar-line icon-xl"
-                aria-hidden="true"
-              />
+          </div>
+          <div className="fr-col-6">
+            <div className="fr-tile">
+              <div className="fr-tile__body">
+                <h5 className="fr-tile__title">
+                  Je peux contacter mes interlocuteurs
+                </h5>
+              </div>
+              <div className="fr-tile__img">
+                <span className="fr-fi-mail-line icon-xl" aria-hidden="true" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="fr-col-6">
-          <div className="fr-tile">
-            <div className="fr-tile__body">
-              <h5 className="fr-tile__title">
-                Je peux contacter mes interlocuteurs
-              </h5>
-            </div>
-            <div className="fr-tile__img">
-              <span className="fr-fi-mail-line icon-xl" aria-hidden="true" />
-            </div>
+
+        <section className="fr-accordion mt-8">
+          <h3 className="fr-accordion__title">
+            <button
+              type="button"
+              className="fr-accordion__btn"
+              aria-expanded="false"
+              aria-controls="accordion-106"
+              data-fr-js-collapse-button="true"
+            >
+              Mes données personnelles sont protégées
+            </button>
+          </h3>
+          <div
+            className="fr-collapse"
+            id="accordion-106"
+            data-fr-js-collapse="true"
+          >
+            La présente interface est à l’initiative du Ministère de la Justice.
+            Le respect de vos droits et de votre vie privée est une priorité. Pour
+            plus d’informations sur l’utilisation de vos données personnelles,
+            vous pouvez vous rendre sur
+            <a target="_blank" href="/donnees_personnelles">
+              la page dédiée
+            </a>
           </div>
-        </div>
+        </section>
       </div>
 
-      <section className="fr-accordion mt-8">
-        <h3 className="fr-accordion__title">
-          <button
-            type="button"
-            className="fr-accordion__btn"
-            aria-expanded="false"
-            aria-controls="accordion-106"
-            data-fr-js-collapse-button="true"
-          >
-            Mes données personnelles sont protégées
-          </button>
-        </h3>
-        <div
-          className="fr-collapse"
-          id="accordion-106"
-          data-fr-js-collapse="true"
-        >
-          La présente interface est à l’initiative du Ministère de la Justice.
-          Le respect de vos droits et de votre vie privée est une priorité. Pour
-          plus d’informations sur l’utilisation de vos données personnelles,
-          vous pouvez vous rendre sur
-          <a target="_blank" href="/donnees_personnelles">
-            la page dédiée
-          </a>
-        </div>
-      </section>
-    </div>
+    </>
+
   );
 }
 
---
+++ src/shared/components/Alerts/Alert.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import dataAttributes from 'shared/utils/data-attributes';
+
+import '@gouvfr/dsfr/dist/component/alert/alert.css';
+
+/**
+ *
+ * @visibleName Alert
+ */
+function Alert({
+  className,
+  as,
+  type,
+  title,
+  description,
+  role,
+  small,
+  show,
+  closable,
+  onClose,
+  ...remainingProps
+}) {
+  const HtmlTitleTag = `${as}`;
+  const [internalShow, setInternalShow] = useState(show);
+  // eslint-disable-next-line no-underscore-dangle
+  const _className = classNames(
+    'fr-alert',
+    `fr-alert--${type}`,
+    {
+      'fr-alert--sm': small,
+    },
+    className,
+  );
+
+  useEffect(() => {
+    setInternalShow(show);
+  }, [show]);
+
+  if (!internalShow) {
+    console.log('on voit pas alerte');
+    return null;
+  }
+
+  return (
+    <div
+      role={role || undefined}
+      className={_className}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...dataAttributes.getAll(remainingProps)}
+    >
+      <HtmlTitleTag className="fr-alert__title">{title}</HtmlTitleTag>
+      {!small && description && <p>{description}</p>}
+      {closable && (
+        <button
+          type="button"
+          className="fr-link--close fr-link"
+          onClick={() => {
+            setInternalShow(false);
+            if (onClose) {
+              onClose();
+            }
+          }}
+        >
+          Masquer le message
+        </button>
+      )}
+    </div>
+  );
+}
+
+Alert.defaultProps = {
+  as: 'p',
+  type: 'info',
+  role: '',
+  description: undefined,
+  small: false,
+  show: true,
+  closable: false,
+  onClose: undefined,
+  className: '',
+};
+
+Alert.propTypes = {
+  as: PropTypes.oneOf(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  title: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]).isRequired,
+  description: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]),
+  type: PropTypes.oneOf(['error', 'success', 'info']),
+  small: PropTypes.bool,
+  role: PropTypes.string,
+  show: PropTypes.bool,
+  closable: PropTypes.bool,
+  onClose: PropTypes.func,
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+  ]),
+};
+
+export default Alert;
