e8521f2e4d7a1b77c2149ae703ec3b274ce75ff9
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-23 14:50:22 +0200

    refacto css for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  /* important true so that tailwind takes precedence over dsfr in production build
+  we want tailwind utilities to have the final say */
+  important: true,
   theme: {
     extend: {},
   },
