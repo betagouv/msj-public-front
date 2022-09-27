ed790da1092e5174cab0756002c45f592e2f01b1
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-27 09:42:30 +0200

    fix tailwind conf for production build

--- src/shared/components/Appointments/AppointmentCard.jsx
+++ src/shared/components/Appointments/AppointmentCard.jsx
@@ -11,22 +11,25 @@ function AppointementCard({ item }) {
   const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
   return (
-    <div className="fr-card fr-card--sm fr-enlarge-link">
-      <div className="fr-card__body">
-        <div className="fr-card__content">
-          <h3 className="fr-card__title">
-            <Link to={`${item.id}`} state={{ appointment: item }}>
-              {`${formattedDate}  à ${time}`}
-            </Link>
-          </h3>
-          <p className="fr-card__desc">{item.place.name}</p>
-          <div className="fr-card__start">
-            <ul className="fr-tags-group">
-              <li>
-                <p className="fr-tag">{item.state}</p>
-              </li>
-            </ul>
-            {/* <p className="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p> */}
+    <div className="col-span-1 fr-card fr-enlarge-link">
+
+      <div className="fr-card fr-card--sm fr-enlarge-link">
+        <div className="fr-card__body">
+          <div className="fr-card__content">
+            <h3 className="fr-card__title">
+              <Link to={`${item.id}`} state={{ appointment: item }}>
+                {`${formattedDate}  à ${time}`}
+              </Link>
+            </h3>
+            <p className="fr-card__desc">{item.place.name}</p>
+            <div className="fr-card__start">
+              <ul className="fr-tags-group">
+                <li>
+                  <p className="fr-tag">{item.state}</p>
+                </li>
+              </ul>
+              {/* <p className="fr-card__detail fr-icon-warning-fill">détail (optionnel)</p> */}
+            </div>
           </div>
         </div>
       </div>
