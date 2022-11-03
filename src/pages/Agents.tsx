7f0cf42e1f03b5f7718aa9537c69f6ab0986155e
Matthieu Faugere <matthieu.faugere@gmail.com>
2022-11-03 17:34:18 +0100

    keep typescripting

--- src/pages/Agents.tsx
+++ src/pages/Agents.tsx
@@ -1,32 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { useAuth } from 'shared/hooks/auth-hook';
-import useHttpClient from 'shared/hooks/http-hook';
+import { useAuth } from "shared/hooks/auth-hook";
+import useHttpClient from "shared/hooks/http-hook";
 
-import Alert from 'shared/components/Alerts/Alert';
+import Alert from "shared/components/Alerts/Alert";
 
+interface Agent {
+  organizationName: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+}
+function agentDataToAgent(agentData: any): Agent {
+  return {
+    organizationName: agentData.organization_name,
+    firstname: agentData.first_name,
+    lastname: agentData.last_name,
+    email: agentData.email,
+  };
+}
 function Agents() {
-  const [agent, setAgent] = useState([]);
+  const [agent, setAgent] = useState<Agent | null>(null);
 
   const { user } = useAuth();
 
-  const {
-    loading, error, sendRequest, clearError,
-  } = useHttpClient();
+  const { loading, error, sendRequest, clearError } = useHttpClient();
 
   useEffect(() => {
     const fetchAgent = async () => {
       try {
         const resData = await sendRequest(
           `${process.env.REACT_APP_BACKEND_HOST}/api/users/${user.msjId}/cpip`,
-          'GET',
+          "GET",
           null,
           {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${user.token}`,
-          },
+          }
         );
-        setAgent(resData);
+        setAgent(agentDataToAgent(resData));
       } catch (err) {
         // TODO: how do we handle errors here ? (They are already handled in the http hook !)
         // eslint-disable-next-line no-console
@@ -37,43 +49,57 @@ function Agents() {
   }, [sendRequest]);
 
   return (
-
     <>
-      <Alert title={error?.message || ''} show={!!error} type="error" closable onClose={clearError} />
+      <Alert
+        title={error?.message || ""}
+        show={!!error}
+        type="error"
+        closable
+        onClose={clearError}
+      />
 
       <div className="fr-container fr-py-6w px-12 lg:px-32">
         <h1 className="text-3xl">Les coordonnées de mes interlocuteurs</h1>
         <div className="bg-white shadow relative mb-8 p-8 lg:p-12 grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2">
-
           {loading && <p>Chargement...</p>}
 
-          {!loading && agent
-      && (
-        <>
-          <div className="sm:col-span-2">
-            <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">Nom du service</h3>
-            <p className="text-md font-bold mb-0">{agent.organization_name}</p>
-          </div>
-          <div className="sm:col-span-2">
-            <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">Mon CPIP référent</h3>
-            <p className="text-md font-bold mb-0">{`${agent.first_name} ${agent.last_name}`}</p>
-          </div>
-          <div className="sm:col-span-1">
-            <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">Email</h3>
-            <a className="text-md font-bold mb-0" href="mailto:johan.goncalves@coleurope.eu">{agent.email}</a>
-          </div>
-        </>
-      )}
-          {!loading && !agent
-        && (
-        <div className="sm:col-span-2">
-          <p className="text-md font-bold mb-0">
-            Vous êtes bien suivi par votre SPIP mais aucun CPIP référent ne
-            vous a été affecté pour l&apos;instant
-          </p>
-        </div>
-        )}
-
+          {!loading && agent && (
+            <>
+              <div className="sm:col-span-2">
+                <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">
+                  Nom du service
+                </h3>
+                <p className="text-md font-bold mb-0">
+                  {agent.organizationName}
+                </p>
+              </div>
+              <div className="sm:col-span-2">
+                <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">
+                  Mon CPIP référent
+                </h3>
+                <p className="text-md font-bold mb-0">{`${agent.firstname} ${agent.lastname}`}</p>
+              </div>
+              <div className="sm:col-span-1">
+                <h3 className="text-xs mb-0 font-medium text-msj-blue uppercase mb-1">
+                  Email
+                </h3>
+                <a
+                  className="text-md font-bold mb-0"
+                  href="mailto:johan.goncalves@coleurope.eu"
+                >
+                  {agent.email}
+                </a>
+              </div>
+            </>
+          )}
+          {!loading && !agent && (
+            <div className="sm:col-span-2">
+              <p className="text-md font-bold mb-0">
+                Vous êtes bien suivi par votre SPIP mais aucun CPIP référent ne
+                vous a été affecté pour l&apos;instant
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </>
--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,67 +1,73 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import useForm from 'shared/hooks/form-hook';
-import TextInput from 'shared/components/Forms/TextInput';
+import useForm from "shared/hooks/form-hook";
+import TextInput from "shared/components/Forms/TextInput";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_FRENCH_MOBILE_NUMBER,
-} from 'shared/utils/validators';
-import useHttpClient from 'shared/hooks/http-hook';
+} from "shared/utils/validators";
+import useHttpClient from "shared/hooks/http-hook";
 
-import Alert from 'shared/components/Alerts/Alert';
+import Alert from "shared/components/Alerts/Alert";
 
 function ForgotPassword() {
-  const {
-    error, sendRequest, clearError,
-  } = useHttpClient();
+  const { error, sendRequest, clearError } = useHttpClient();
 
   const navigate = useNavigate();
 
-  const [formState, inputHandler] = useForm({
-    phone: {
-      value: '',
-      isValid: false,
+  const [formState, inputHandler] = useForm(
+    {
+      phone: {
+        value: "",
+        isValid: false,
+      },
     },
-  }, false);
+    false
+  );
 
   const loginSubmitHandler = async (e) => {
     e.preventDefault();
 
     await sendRequest(
       `${process.env.REACT_APP_BACKEND_HOST}/api/users/reset-password`,
-      'POST',
+      "POST",
       JSON.stringify({
         phone: formState.inputs.phone.value,
       }),
       {
-        'Content-Type': 'application/json',
-      },
+        "Content-Type": "application/json",
+      }
     );
-    navigate('/connexion', { state: { alertSuccess: 'Un SMS avec un lien de réinitialisation de votre mot de passe vous a été envoyé' } });
+    navigate("/connexion", {
+      state: {
+        alertSuccess:
+          "Un SMS avec un lien de réinitialisation de votre mot de passe vous a été envoyé",
+      },
+    });
     // TODO: how do we handle errors here ? (They are already handled in the http hook !)
   };
 
   return (
-
     <>
-      <Alert title={error?.message} show={!!error} type="error" closable onClose={clearError} />
+      <Alert
+        title={error?.message}
+        show={!!error}
+        type="error"
+        closable
+        onClose={clearError}
+      />
       <div className="fr-container fr-py-6w px-12 lg:px-72">
         <h2 className="mb-4">J&apos;ai oublié mon mot de passe ?</h2>
-        <form
-          acceptCharset="UTF-8"
-          onSubmit={loginSubmitHandler}
-        >
+        <form acceptCharset="UTF-8" onSubmit={loginSubmitHandler}>
           <TextInput
             label="Numéro de téléphone"
             type="tel"
             id="phone"
             required
             hint="En 06 ou 07 (ex: 0612131415 ou 0612131415)"
-            autoComplete="tel"
             onInput={inputHandler}
-            errorMessage="Veuillez saisir un numéro de téléphone valide"
-            maxlength="10"
+            maxlength={10}
             validators={[VALIDATOR_REQUIRE(), VALIDATOR_FRENCH_MOBILE_NUMBER()]}
           />
 
@@ -78,11 +84,8 @@ function ForgotPassword() {
 
         <Link to="/connexion">Ecran de connexion</Link>
         <br />
-
       </div>
-
     </>
-
   );
 }
 
