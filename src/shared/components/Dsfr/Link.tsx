6eb33ddb6404dbe009c0c1027f578908db327ef2
Matthieu Faugere <matthieu.faugere@gmail.com>
2022-11-04 14:43:34 +0100

    type Link

--- src/shared/components/Dsfr/Link.tsx
+++ src/shared/components/Dsfr/Link.tsx
@@ -1,59 +1,89 @@
-import React, { cloneElement } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Icon } from './Icon';
+import React, { cloneElement } from "react";
+import classNames from "classnames";
+import { Icon } from "./Icon";
+
+interface LinkProps {
+  className?: classNames.Argument;
+  children: JSX.Element;
+  href?: string;
+  disabled?: boolean;
+  verticalIconPosition?: "top" | "middle |sub";
+  as?: JSX.Element;
+  title?: string;
+  target?: string;
+  isSimple?: boolean;
+  size?: "sm" | "md" | "lg";
+  current?: boolean;
+  icon?: string;
+  onClick?: (e: Event) => void;
+  iconPosition?: "left" | "right";
+  iconSize?:
+    | "fw"
+    | "xxs"
+    | "xs"
+    | "sm"
+    | "1x"
+    | "lg"
+    | "xl"
+    | "2x"
+    | "3x"
+    | "10x";
+  display?: "inline" | "flex";
+}
 
 /**
  *
  * @visibleName Link
  */
-function Link({
-  children,
-  disabled,
-  href,
-  title,
-  target,
-  isSimple,
-  className: defaultClassname,
-  as,
-  current,
-  display,
-  verticalIconPosition,
-  iconSize,
-  icon,
-  iconPosition,
-  onClick,
-  size,
-}) {
+function Link(props: LinkProps) {
+  const {
+    className: defaultClassname = "",
+    disabled = false,
+    title = "",
+    target = "_self",
+    isSimple = false,
+    current = false,
+    icon = "",
+    as = null,
+    iconPosition = "right",
+    href = "",
+    children = "",
+    display = "inline",
+    verticalIconPosition = "middle",
+    iconSize = "sm",
+    size = "md",
+    onClick,
+  } = props;
+
   const className = classNames(defaultClassname, {
     [`ds-fr--${display}`]: display && icon,
-    'fr-link': isSimple,
+    "fr-link": isSimple,
     [`fr-link--${size}`]: size,
   });
   const onClickLink = (e) => {
     e.preventDefault();
-    onClick(e);
+    onClick?.(e);
   };
 
   const asLink = as
     ? cloneElement(as, {
-      className,
-      children,
-      'aria-current': (current && 'page') || undefined,
-      onClick,
-    })
+        className,
+        children,
+        "aria-current": (current && "page") || undefined,
+        onClick,
+      })
     : null;
 
   const linkElement = (
     <a
-      aria-disabled={(disabled || !href) ? true : undefined}
-      role={disabled || !href ? 'link' : undefined}
+      aria-disabled={disabled || !href ? true : undefined}
+      role={disabled || !href ? "link" : undefined}
       onClick={onClick ? (e) => onClickLink(e) : undefined}
-      aria-current={current ? 'page' : undefined}
+      aria-current={current ? "page" : undefined}
       href={href || undefined}
       title={title || undefined}
       target={target}
-      rel={(target === '_blank') ? 'noopener noreferrer' : undefined}
+      rel={target === "_blank" ? "noopener noreferrer" : undefined}
       className={className}
     >
       {children}
@@ -65,60 +95,15 @@ function Link({
       verticalAlign={verticalIconPosition}
       name={icon}
       size={iconSize}
-      iconPosition={element.props && element.props.children ? iconPosition : 'center'}
+      iconPosition={
+        element.props && element.props.children ? iconPosition : "center"
+      }
     >
       {element}
     </Icon>
-  ) : element;
+  ) : (
+    element
+  );
 }
-
-Link.defaultProps = {
-  className: '',
-  disabled: false,
-  title: '',
-  target: '_self',
-  isSimple: false,
-  current: false,
-  icon: '',
-  as: null,
-  iconPosition: 'right',
-  href: '',
-  children: '',
-  onClick: null,
-  display: 'inline',
-  verticalIconPosition: 'middle',
-  iconSize: 'sm',
-  size: 'md',
-};
-
-Link.propTypes = {
-  className: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.object,
-    PropTypes.array,
-  ]),
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-    PropTypes.string,
-  ]),
-  href: PropTypes.string,
-  disabled: PropTypes.bool,
-  verticalIconPosition: PropTypes.oneOf(['top', 'middle', 'sub']),
-  as: PropTypes.element,
-  title: PropTypes.string,
-  target: PropTypes.string,
-  isSimple: PropTypes.bool,
-  size: PropTypes.oneOf(['sm', 'md', 'lg']),
-  current: PropTypes.bool,
-  icon: PropTypes.string,
-  onClick: PropTypes.func,
-  iconPosition: PropTypes.oneOf(['left', 'right']),
-  iconSize: PropTypes.oneOf(['fw', 'xxs', 'xs', 'sm', '1x', 'lg', 'xl', '2x', '3x', '10x']),
-  /**
-     * @ignore
-     */
-  display: PropTypes.oneOf(['inline', 'flex']),
-};
 
 export default Link;
