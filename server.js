2f121a948bd07be759de59c1a75152b9c8d11ddc
Charles Marcoin <charles.marcoin@gmail.com>
2024-01-30 17:42:37 +0100

    added a csp

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
 const directory = `/${process.env.STATIC_DIR || 'dist'}`;
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      connectSrc: ["'self'", process.env.REACT_APP_BACKEND_HOST],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", 'data:'],
+      fontSrc: ["'self'"],
+    },
+  },
+}));
+
 app.use(express.static(path.join(__dirname, directory)));
 
 // We want to use browserHistory
